484861d06751a8b380178ba1c50e9eb1b59428f2
kingdave4 <131571494+kingdave4@users.noreply.github.com>
2025-04-28 15:15:27 -0400

    Update custom_role.tf

--- Terraform/custom_role.tf
+++ Terraform/custom_role.tf
@@ -1,36 +1,7 @@
 data "azurerm_subscription" "primary" {}
 
-resource "random_uuid" "role_def_id" {}
-
-resource "azurerm_role_definition" "github_ci_cd" {
+data "azurerm_role_definition" "github_ci_cd" {
   name        = "GitHub-CICD-Terraform-Role"
   scope       = data.azurerm_subscription.primary.id
-  role_definition_id = random_uuid.role_def_id.result
   description = "Custom role for GitHub Actions to deploy infrastructure and monitor logs."
-
-  permissions {
-    actions = [
-      "Microsoft.Resources/subscriptions/resourceGroups/*",
-      "Microsoft.Resources/deployments/*",
-      "Microsoft.Compute/*/read",
-      "Microsoft.Network/*/read",
-      "Microsoft.Web/sites/*",
-      "Microsoft.Web/serverfarms/*",
-      "Microsoft.KeyVault/vaults/*",
-      "Microsoft.KeyVault/vaults/secrets/*",
-      "Microsoft.Storage/storageAccounts/*",
-      "Microsoft.Authorization/roleAssignments/*",
-      "Microsoft.Insights/components/*",
-      "Microsoft.Insights/metrics/*",
-      "Microsoft.Insights/eventtypes/*",
-      "Microsoft.OperationalInsights/workspaces/*",
-      "Microsoft.OperationalInsights/workspaces/api/query/action"
-    ]
-
-    not_actions = []
-  }
-
-  assignable_scopes = [
-    data.azurerm_subscription.primary.id
-  ]
 }
