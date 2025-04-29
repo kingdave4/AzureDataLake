a7a33e8c6d4c04370aa326cb152d6780988f59cd
kingdave4 <131571494+kingdave4@users.noreply.github.com>
2025-04-29 13:08:06 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -87,7 +87,7 @@ resource "azurerm_key_vault_access_policy" "terraform_sp" {
   for_each            = local.access_policies
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  object_id    = each.key
 
   secret_permissions = [
     "Get",
